ca4d1f381511bd8bfe7259ee8cdbea7e37323481
kimjy-par <kimjy.par@gmail.com>
2022-05-31 09:57:16 +0900

    add part 10

--- part-5/arg_func.exs
+++ part-5/arg_func.exs
@@ -3,3 +3,4 @@ times_2 = fn n -> n *2 end
 apply = fn (func, value) -> func.(value) end
 
 IO.puts apply.(times_2, 2)
+
--- part-8/access3.exs
+++ part-8/access3.exs
@@ -11,4 +11,4 @@ cast = %{
 
 IO.inspect get_in(cast, [Access.key(:westley), :actor, Access.elem(1)])
 IO.inspect get_and_update_in(cast, [Access.key(:buttercup), :role],
-                            fn (val)-> {val, "Queen"} end)
+                            fn (val)-> {val, "Queen"} end)0
