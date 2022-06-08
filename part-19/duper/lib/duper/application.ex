b33cb35e28798ef72f55fba43fa94c88f27cbf3c
kimjy-par <kimjy.par@gmail.com>
2022-06-08 12:03:59 +0900

    add part 19

--- part-19/duper/lib/duper/application.ex
+++ part-19/duper/lib/duper/application.ex
@@ -9,6 +9,9 @@ defmodule Duper.Application do
   def start(_type, _args) do
     children = [
       Duper.Results,
+      {Duper.PathFinder, "."},
+      Duper.WorkerSupervisor,
+      {Duper.Gatherer,     1}
       # Starts a worker by calling: Duper.Worker.start_link(arg)
       # {Duper.Worker, arg}
     ]
